1afcba2e2da582839203fc7cbdf46e82f15e9fe4
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-06-12 23:29:40 -0300

    ADDED: Grains.

---
+++ grains/grains.c
@@ -0,0 +1,15 @@
+#include "grains.h"
+
+uint64_t square(uint8_t index)
+{
+    return pow(2, index - 1);
+}
+uint64_t total(void)
+{
+    uint64_t sum = 0;
+
+    for(uint8_t i = 1; i <= 64;i++)
+        sum += square(i);
+    
+    return sum;
+}
